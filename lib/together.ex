fae90332d3b780792f8949ccd1a056f30a2a63b0
Po Chen <chenpaul914@gmail.com>
2016-12-10 21:47:42 +1100

    fix typespec

--- lib/together.ex
+++ lib/together.ex
@@ -1,10 +1,12 @@
 defmodule Together do
-  @spec process(atom | pid, term, function)
+  @spec process(atom | pid, term, fun) ::
+    {:ok, pid} | :ignore | {:error, {:already_started, pid} | term}
   def process(pid \\ Together.Server, id, func) do
     GenServer.call(pid, {:process, id, func})
   end
 
-  @spec process(atom | pid, term, atom, atom, list)
+  @spec process(atom | pid, term, module, atom, list) ::
+    {:ok, pid} | :ignore | {:error, {:already_started, pid} | term}
   def process(pid \\ Together.Server, id, m, f, a) do
     GenServer.call(pid, {:process, id, {m, f, a}})
   end
