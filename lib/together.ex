58907998eafea84ca695f92c8fced53d8b96ebce
Po Chen <chenpaul914@gmail.com>
2017-02-22 12:25:50 +1100

    improved docs

--- lib/together.ex
+++ lib/together.ex
@@ -1,12 +1,16 @@
 defmodule Together do
   @moduledoc ~S"""
-  Group actions that need to be performed later together
+  Group actions that can be handled / responded to later together
 
   ## What for?
 
-  - group notifications before sending an email about them
-  - only sending the very last value of a fast changing entity (renew: true)
-  - only using the very first value of a changing entity in every fixed period (renew: false)
+  - group notifications to be sent in ONE email
+    - cancel the previously queued email if another event happens within a short period (type: debounce)
+  - make heavy operations happen less often, i.e. refresh some global statistics
+    - allow only 1 operation per certain period (type: throttle)
+  - protect some write api
+    - additonally you can choose to use the first value in a period (keep: first)
+    - or the last value in the period (keep: last)
 
   ## How to use
 
@@ -14,21 +18,17 @@ defmodule Together do
 
   You can start it by adding a worker to your app's supervision tree
 
-  ```elixir
-  worker(Together.Worker, [[delay: 3000, type: :debounce], [name: Together.Worker]])
-  ```
+      worker(Together.Worker, [[delay: 3000, type: :debounce], [name: Together.Worker]])
 
   Or start it as you would any other GenServer
 
-  ```elixir
-  {:ok, pid} = Together.Worker.start_link(delay: 30_000, type: :throttle)
-  ```
+      {:ok, pid} = Together.Worker.start_link(delay: 30_000, type: :throttle)
 
   Make calls to the worker process:
 
-  - `Together.process(binary_name, "somethiny_unique", some_func)`
-  - `Together.process(pid, "some_unique_name_or_id", a_function)`
-  - `Together.process(Together.Worker, "id", Module, :func, [arg1, arg2, ...])`
+      Together.process(binary_name, "somethiny_unique", some_func)
+      Together.process(pid, "some_unique_name_or_id", a_function)
+      Together.process(Together.Worker, "id", Module, :func, [arg1, arg2, ...])
   """
 
   @registry_name Together.WorkerRegistry
@@ -45,7 +45,7 @@ defmodule Together do
   end
 
   @doc ~S"""
-  put in an `mfa` under the id to be processed (invoked/applied) later
+  put in an `mfa` under the id to be processed (applied) later
   """
   @spec process(binary | GenServer.server, term, module, atom, list) :: :ok | no_return
   def process(name, id, m, f, a) when is_binary(name) do
@@ -56,7 +56,7 @@ defmodule Together do
   end
 
   @doc ~S"""
-  cancels queued action(s) for the given id
+  cancels queued action for the given id
   """
   @spec cancel(binary | GenServer.server, term) :: :ok | :error
   def cancel(name, id) when is_binary(name) do
--- lib/together/proxy.ex
+++ lib/together/proxy.ex
@@ -1,7 +1,19 @@
 defmodule Together.Proxy do
+  @moduledoc """
+  Proxy is here to receive all the `Process.send_after` messages
+
+  It offloads the message handling from worker, and makes sure
+  that if the worker dies, the respawned worker will still get
+  the messages, since Proxy calls Worker by name instead of pid
+
+  If the Proxy dies, it will lose all the messages, but the Proxy is
+  less likely to die than Worker, because it has basically no logic
+  and no interaction with outside `Together`
+  """
   use GenServer
   use Together.Global, :start_link
 
+  @doc false
   def queue(pid \\ __MODULE__, id, delay) do
     GenServer.call(pid, {:queue, id, delay})
   end
